7f102193e00da9809e3d83fa9d9cf3e116bec8e0
bear109 <jokerbear109@gmail.com>
2021-07-13 15:12:18 +0900

    ch14-커스텀 훅

--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -1,7 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import axios from '../../node_modules/axios/index';
 import NewsItem from './NewsItem';
+import usePromise from '../lib/usePromise';
 
 const NewsListBlock = styled.div`
   box-sizing: border-box;
@@ -17,34 +18,25 @@ const NewsListBlock = styled.div`
 `;
 
 const NewsList = ({ category }) => {
-  const [articles, setArticles] = useState(null);
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      setLoading(true);
-      try {
-        const query = category === 'all' ? '' : `&category=${category}`;
-        const response = await axios.get(
-          `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=de474c033b2d409eb51bb1da223be6cf`
-        );
-        setArticles(response.data.articles);
-      } catch (e) {
-        console.log(e);
-      }
-      setLoading(false);
-    };
-    fetchData();
+  const [loading, response, error] = usePromise(() => {
+    const query = category === 'all' ? '' : `&category=${category}`;
+    return axios.get(
+      `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=de474c033b2d409eb51bb1da223be6cf`
+    );
   }, [category]);
 
   if (loading) {
     return <NewsListBlock>대기중 ...</NewsListBlock>;
   }
 
-  if (!articles) {
+  if (!response) {
     return null;
   }
+  if (error) {
+    return <NewsListBlock>에러 발생!</NewsListBlock>;
+  }
 
+  const { articles } = response.data;
   return (
     <NewsListBlock>
       {articles.map((article) => (
---
+++ src/lib/usePromise.js
@@ -0,0 +1,23 @@
+import { useEffect, useState } from 'react';
+
+export default function usePromise(promiseCreator, deps) {
+  const [loading, setLoading] = useState(false);
+  const [resolved, setResloved] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const process = async () => {
+      setLoading(true);
+      try {
+        const resolved = await promiseCreator();
+        setResloved(resolved);
+      } catch (e) {
+        setError(e);
+      }
+      setLoading(false);
+    };
+    process();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, deps);
+  return [loading, resolved, error];
+}
